d323fc3a1cf519397996879bfc14a8c272dead93
Pirker Alexander <pirker_alexander@hotmail.com>
2021-06-21 13:45:51 +0200

    creation of service chassis project, moving all the service code to a subfolder

---
+++ eShoppingSimple.ServiceChassis/eShoppingSimple.ServiceChassis.Storage.Abstractions/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace eShoppingSimple.ServiceChassis.Storage.Abstractions
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T item);
+
+        void Delete(T item);
+
+        void Update(T item);
+
+        IEnumerable<T> GetAll();
+    }
+}
